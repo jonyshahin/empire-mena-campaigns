15287e4f0491e2820f2071a06d464ba2ed207013
Johnny Shahin <jony.shahin@gmail.com>
2024-05-08 22:55:02 +0300

    zone seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -19,7 +19,7 @@ class DatabaseSeeder extends Seeder
                 CompetitorBrandSeeder::class,
                 RefusedReasonSeeder::class,
                 DistrictSeeder::class,
-                OutletsSeeder::class,
+                ZoneSeeder::class,
             ]
         );
     }
