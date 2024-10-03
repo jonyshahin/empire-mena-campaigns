fced0719e51ea133f96593dafa55dab8ee1e7446
= <=>
2024-10-03 14:51:20 +0300

    get team leader campaign fix bug

--- app/Http/Controllers/API/CampaignController.php
+++ app/Http/Controllers/API/CampaignController.php
@@ -154,7 +154,7 @@ class CampaignController extends Controller
                 return custom_error(403, 'You are not authorized to access this resource');
             }
 
-            $campaigns = $user->campaigns()->pluck('campaign_id');
+            $campaigns = $user->team_leader_campaigns()->pluck('campaign_id');
 
             $models = QueryBuilder::for(Campaign::class)
                 ->whereIn('id', $campaigns)
