9c5f0265dfdf1d436346db91f02f9a1b5dbc9a55
= <=>
2024-11-14 12:54:50 +0300

    fix bug

--- app/Exports/ConsumersByPromoterExport.php
+++ app/Exports/ConsumersByPromoterExport.php
@@ -83,7 +83,7 @@ class ConsumersByPromoterExport implements FromCollection, WithHeadings
                     'Incentives' => $consumer->incentives,
                     'Franchise' => $consumer->franchise ? 'Yes' : 'No',
                     'Did He Switch' => $consumer->did_he_switch ? 'Yes' : 'No',
-                    'Competitor Brand' => optional($consumer->competitor_product->brand)->name,
+                    'Competitor Brand' => optional($consumer->competitor_product)->brand->name,
                     'Competitor Product' => optional($consumer->competitor_product)->name,
                     // 'Other Brand Name' => $consumer->other_brand_name == null ? '' : $consumer->other_brand_name,
                     'Products' => $consumer->selected_products,
