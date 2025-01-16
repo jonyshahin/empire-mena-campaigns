1c3837ac060e01661924b5268525eabd862db772
= <=>
2025-01-16 17:47:33 +0300

    fix bug

--- app/Http/Controllers/API/ReportController.php
+++ app/Http/Controllers/API/ReportController.php
@@ -49,10 +49,16 @@ class ReportController extends Controller
                             'check_in_time' => $attendance_record->check_in_time,
                             'check_out_time' => $attendance_record->check_out_time,
                             'last_day_note' => $attendance_record->last_day_note,
-                            'stock_first' => $attendance_record->stock_first->map(function ($stock_first_record) {
+                            'stock_first' => $attendance_record->stock_first->array_map(function ($stock_first_record) {
                                 return [
                                     'product_name' => $stock_first_record->product_name,
                                     'stock' => $stock_first_record->stock,
+                                ];
+                            }),
+                            'stock_last' => $attendance_record->stock_last->array_map(function ($stock_last_record) {
+                                return [
+                                    'product_name' => $stock_last_record->product_name,
+                                    'stock' => $stock_last_record->stock,
                                 ];
                             }),
                             'created_at' => Carbon::parse($attendance_record->created_at)->timezone($timezone)->toDateTimeString(),
