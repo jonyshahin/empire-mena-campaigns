ba821ce117df952b847e4d7ef07c3ce59637a9bd
= <=>
2025-01-16 17:43:36 +0300

    stock report

--- app/Http/Controllers/API/ReportController.php
+++ app/Http/Controllers/API/ReportController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\API;
 use App\Http\Controllers\Controller;
 use App\Models\AttendanceRecord;
 use App\Models\User;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
@@ -29,12 +30,39 @@ class ReportController extends Controller
             $outletId = $request->input('outlet_id');
             $campaign_id = $request->input('campaign_id');
 
+            $timezone = 'Asia/Baghdad';
+
             $campaign_attendance_records = AttendanceRecord::where('campaign_id', $campaign_id)
-                ->with(['user'])
+                ->with([
+                    'user',
+                    'outlet'
+                ])
                 ->get()
                 ->groupBy('user.name');
 
-            return custom_success(200, 'Report generated successfully', $campaign_attendance_records);
+            $reportData = $campaign_attendance_records->map(function ($attendance_records, $promoterName) use ($timezone) {
+                return [
+                    'promoter' => $promoterName,
+                    'attendance_records' => $attendance_records->map(function ($attendance_record) use ($timezone) {
+                        return [
+                            'outlet' => $attendance_record->outlet->name,
+                            'check_in_time' => $attendance_record->check_in_time,
+                            'check_out_time' => $attendance_record->check_out_time,
+                            'last_day_note' => $attendance_record->last_day_note,
+                            'stock_first' => $attendance_record->stock_first->map(function ($stock_first_record) {
+                                return [
+                                    'product_name' => $stock_first_record->product_name,
+                                    'stock' => $stock_first_record->stock,
+                                ];
+                            }),
+                            'created_at' => Carbon::parse($attendance_record->created_at)->timezone($timezone)->toDateTimeString(),
+                            'updated_at' => Carbon::parse($attendance_record->updated_at)->timezone($timezone)->toDateTimeString(),
+                        ];
+                    }),
+                ];
+            });
+
+            return custom_success(200, 'Report generated successfully', $reportData);
         } catch (\Throwable $th) {
             return custom_error(500, $th->getMessage());
         }
