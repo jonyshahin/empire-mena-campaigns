c57e36a2fa2d6a7949b9b72dd2724a1d5b54a60b
= <=>
2025-03-06 21:32:24 +0300

    activate whatsapp otp with consumer form

--- app/Http/Controllers/API/ConsumerController.php
+++ app/Http/Controllers/API/ConsumerController.php
@@ -76,19 +76,19 @@ class ConsumerController extends Controller
                 'dynamic_incentives.*.id' => 'required|integer|exists:incentives,id',
             ]);
 
-            // $otp_code = rand(100000, 999999);
-            // $otp_expired_at = Carbon::now()->addMinutes(15);
+            $otp_code = rand(100000, 999999);
+            $otp_expired_at = Carbon::now()->addMinutes(15);
 
-            // /*************** Send WhatsApp code********************/
-            // if ($request->filled('telephone')) {
-            //     $phone = $request->telephone;
-            //     $whatsapp_data = whatsapp_message($phone, $otp_code);
+            /*************** Send WhatsApp code********************/
+            if ($request->filled('telephone')) {
+                $phone = $request->telephone;
+                $whatsapp_data = whatsapp_message($phone, $otp_code);
 
-            //     if ($whatsapp_data['code'] == 0) {
-            //         return custom_error(400, $whatsapp_data['data']);
-            //     }
-            // }
-            // /******************************************************/
+                if ($whatsapp_data['code'] == 0) {
+                    return custom_error(400, $whatsapp_data['data']);
+                }
+            }
+            /******************************************************/
 
             $user = User::find(auth()->id());
 
@@ -150,16 +150,16 @@ class ConsumerController extends Controller
             $otp_code = rand(100000, 999999);
             $otp_expired_at = Carbon::now()->addMinutes(15);
 
-            // /*************** Send WhatsApp code********************/
-            // if ($request->filled('telephone')) {
-            //     $phone = $request->telephone;
-            //     $whatsapp_data = whatsapp_message($phone, $otp_code);
+            /*************** Send WhatsApp code********************/
+            if ($request->filled('telephone')) {
+                $phone = $request->telephone;
+                $whatsapp_data = whatsapp_message($phone, $otp_code);
 
-            //     if ($whatsapp_data['code'] == 0) {
-            //         return custom_error(400, $whatsapp_data['data']);
-            //     }
-            // }
-            // /******************************************************/
+                if ($whatsapp_data['code'] == 0) {
+                    return custom_error(400, $whatsapp_data['data']);
+                }
+            }
+            /******************************************************/
 
             return custom_success(200, 'OTP sent successfully', $otp_code);
         } catch (\Throwable $th) {
@@ -221,17 +221,17 @@ class ConsumerController extends Controller
             if (!$consumer) {
                 return custom_error(404, 'Consumer not found');
             }
-            // $otp_code = rand(100000, 999999);
-            // $otp_expired_at = Carbon::now()->addMinutes(15);
-            // /*************** Send WhatsApp code********************/
-            // if ($request->filled('telephone')) {
-            //     $phone = $request->telephone;
-            //     $whatsapp_data = whatsapp_message($phone, $otp_code);
+            $otp_code = rand(100000, 999999);
+            $otp_expired_at = Carbon::now()->addMinutes(15);
+            /*************** Send WhatsApp code********************/
+            if ($request->filled('telephone')) {
+                $phone = $request->telephone;
+                $whatsapp_data = whatsapp_message($phone, $otp_code);
 
-            //     if ($whatsapp_data['code'] == 0) {
-            //         return custom_error(400, $whatsapp_data['data']);
-            //     }
-            // }
+                if ($whatsapp_data['code'] == 0) {
+                    return custom_error(400, $whatsapp_data['data']);
+                }
+            }
             /******************************************************/
 
             $packs = 0;
