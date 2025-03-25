4108b181897fe2696e2798c966b4f63bd595ada2
= <=>
2025-03-25 20:28:13 +0300

    update email to hanna gmail

--- app/Http/Controllers/API/ConsumerController.php
+++ app/Http/Controllers/API/ConsumerController.php
@@ -610,8 +610,8 @@ class ConsumerController extends Controller
             $competitor_product_ids,
             $totalCount
         ), $fileName)->chain([
+            new SendExportEmail('hannasaad923@gmail.com', $filePath),
             new SendExportEmail('jony.shahin@gmail.com', $filePath),
-            new SendExportEmail('h.saad@empire-mena.com', $filePath),
         ]);
 
         return custom_success(200, 'Export started. You will receive an email once it is ready.', []);
